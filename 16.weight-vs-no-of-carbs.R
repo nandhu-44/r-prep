6d3e9a10319298551464572d949e91fcb8a2b907
Nandhu Krishnan A <91179000+nandhu-44@users.noreply.github.com>
2024-12-12 05:54:04 +0530

    Create 16.weight-vs-no-of-carbs.R

---
+++ 16.weight-vs-no-of-carbs.R
@@ -0,0 +1,7 @@
+data(mtcars)
+carbs <- as.factor(mtcars$carb)
+plot(mtcars$disp, mtcars$wt, col = carbs, main = "Scatterplot of Displacement vs Weight", xlab = "Displacement", ylab = "Weight", pch = 21)
+legend(x = "topleft", legend = levels(carbs), horiz = TRUE, col = 1:length(levels(carbs)), pch = 15, title = "Number of Carburetors")
+loess_fit <- loess(wt ~ disp, data = mtcars)
+disp_sorted <- sort(mtcars$disp)
+lines(disp_sorted, predict(loess_fit, newdata = data.frame(disp = disp_sorted)), lwd = 2, col = "darkolivegreen")
